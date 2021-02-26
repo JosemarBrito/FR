7ad6d8d171020efa8ae323fcf1f705f7f3841930
Josemar Brito <estudos.josemarbrito@gmail.com>
2021-02-26 20:37:33 -0300

    Criado pagina inicial close #5

--- loja/base/views.py
+++ loja/base/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def home(request):
-    return HttpResponse('Filhas do Rei')
+    return HttpResponse('Filhas do Rei, o melhor lugar para voc~e compras suas roupas evangelicas'
+                        'Venha conferir')
